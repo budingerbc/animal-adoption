f6c0d6b14c33232c5b994606be297a5c2797861c
Ben Budinger, Jonathan Stein, LaTanya Donaldson, Martha Gonzalez and Sharon Gray <jonathan@>
2017-07-17 19:20:54 -0700

    create basic layout for animal shelter project

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+//section 1-animal
+
+//end section 1
+
+//section2- matchmaker
+
+//end section 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  //section 1-animal
+
+  //end section 1
+
+  //section2- matchmaker
+
+  //end section 2
+});
